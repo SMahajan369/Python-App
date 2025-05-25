f5cf8c548f6f229783d0f7017fbe069d746946c4
SMahajan369 <SMLata369@gmail.com>
2025-05-25 18:01:23 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,7 @@
-# main.py
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class HelloRequest(BaseModel):
-    name: str
-
-@app.get("/hello")
-def read_hello(name: str = "World"):
-    return {"message": f"Hello, {name}!"}
-
-@app.post("/hello")
-def create_hello(request: HelloRequest):
-    return {"message": f"Hello, {request.name}!"}
+@app.get("/")
+def read_root():
+    return {"message": "Hello, world!"}
